3896706e40ece2dd1893e9dbcb5982d81da67cb9
nirwan1265 <tandukar.nirwan@gmail.com>
2023-09-22 11:17:47 -0400

    update

--- scripts/load_data.R
+++ scripts/load_data.R
@@ -6,7 +6,7 @@ library(dplyr)
 #https://academic.oup.com/bib/article/24/1/bbac572/6966533
 
 ## Load data
-lipids <- read.csv("data/tidy_data_AB.csv")
+lipids <- read.csv("data/filtered_tidy_data_AB.csv", check.names = FALSE)
 names(lipids)
 unique(lipids$genotype)
 
@@ -14,18 +14,22 @@ PT <- as.data.frame(lipids[which(lipids$genotype == "PT"), ])
 CML312 <- as.data.frame(lipids[which(lipids$genotype == "CML312"), ])
 B73 <- as.data.frame(lipids[which(lipids$genotype == "B73"), ])
 
-
+names(PT)
 # Filtering for developmental stage
-PT$developmental_stage <- paste0("D_", PT$developmental_stage) 
-PT <- PT[,c(1,6,9:262)]
+#PT$developmental_stage <- paste0("DevStg_", PT$developmental_stage) 
+PT$leaf <- paste0("Leaf_", PT$leaf) 
+PT <- PT[,c(1,7,9:159)]
 PT_data <- PT[,-2]
 colnames(PT_data)[1] <- "samples"
+names(PT_data)
+
 PT_group <- PT[,c(1,2)]
 colnames(PT_group)[1:2] <- c("sample","group")
 
 
-CML312$developmental_stage <- paste0("D_", CML312$developmental_stage) 
-CML312 <- CML312[,c(1,6,9:262)]
+#CML312$developmental_stage <- paste0("DevStg_", CML312$developmental_stage) 
+CML312$leaf <- paste0("Leaf_", CML312$leaf) 
+CML312 <- CML312[,c(1,7,9:159)]
 CML312_data <- CML312[,-2]
 colnames(CML312_data)[1] <- "samples"
 CML312_group <- CML312[,c(1,2)]
@@ -33,8 +37,9 @@ colnames(CML312_group)[1:2] <- c("sample","group")
 
 
 
-B73$developmental_stage <- paste0("D_", B73$developmental_stage) 
-B73 <- B73[,c(1,6,9:262)]
+#B73$developmental_stage <- paste0("DevStg_", B73$developmental_stage) 
+B73$leaf <- paste0("Leaf_", B73$leaf) 
+B73 <- B73[,c(1,7,9:159)]
 B73_data <- B73[,-2]
 colnames(B73_data)[1] <- "samples"
 B73_group <- B73[,c(1,2)]
@@ -47,4 +52,5 @@ write.csv(CML312_data,"data/CML312_data.csv", row.names = F)
 write.csv(CML312_group,"data/CML312_group.csv", row.names = F)
 write.csv(B73_data,"data/B73_data.csv", row.names = F)
 write.csv(B73_group,"data/B73_group.csv", row.names = F)
+
 
